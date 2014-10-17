9aaf874f9d8918c64f7e8b2d215d38100305bd11
一个有双下巴的死胖子 <zhaoshuxiang2010@gmail.com>
2014-10-18 00:00:45 +0800

    建立代码库
    建立代码库（Java实现）

---
+++ LeetCode/src/com/leetcode/MaxProSubArr/Solution.java
@@ -0,0 +1,14 @@
+package com.leetcode.MaxProSubArr;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		int testData[] = { 2, 3, -2, 4 };
+		
+		System.out.println("");
+	}
+
+    public String reverseWords(String s) {
+        return "";
+    }
+}
---
+++ LeetCode/src/com/leetcode/ReverseWords/Solution.java
@@ -0,0 +1,12 @@
+package com.leetcode.ReverseWords;
+
+public class Solution {
+	public static void main(String[] args) {
+		int testData[] = { 2, 3, -2, 4 };
+		System.out.println("");
+	}
+
+	public static int maxProduct(int[] A) {
+		return 1;
+	}
+}
