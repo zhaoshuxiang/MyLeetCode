d2e8ec52a6e5026973d52bfa6c97630b8eb9c993
一个有双下巴的死胖子 <zhaoshuxiang2010@gmail.com>
2014-11-07 18:41:14 +0800

    链表的排序，反转，插入
    链表的排序，反转，插入

---
+++ LeetCode/src/com/leetcode/PartitionList/ListNode.java
@@ -0,0 +1,11 @@
+package com.leetcode.PartitionList;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
---
+++ LeetCode/src/com/leetcode/ReorderList/ListNode.java
@@ -0,0 +1,11 @@
+package com.leetcode.ReorderList;
+
+public class ListNode {
+	int val;
+	ListNode next;
+
+	ListNode(int x) {
+		val = x;
+		next = null;
+	}
+}
